ca8d49314037c7fe69bbf19779bc66c685fc47f0
szhou <szhou@xtremeprog.com>
2015-03-19 10:43:57 +0800

    add xml update activity(addsub & editgroup)

--- src/com/gizwits/ledgateway/activity/AddSubDeviceActivity.java
+++ src/com/gizwits/ledgateway/activity/AddSubDeviceActivity.java
@@ -1,7 +1,49 @@
 package com.gizwits.ledgateway.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import com.gizwits.framework.activity.BaseActivity;
+import com.gizwits.ledgateway.R;
 
-public class AddSubDeviceActivity extends BaseActivity{
+public class AddSubDeviceActivity extends BaseActivity implements OnClickListener{
 
+	/** The btn confirm. */
+	private Button confirm_btn;
+	/** The iv back. */
+	private ImageView ivBack;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_sub);
+		initViews();
+		initEvent();
+	}
+	
+	private void initViews(){
+		confirm_btn = (Button) findViewById(R.id.confirm_btn);
+		ivBack = (ImageView) findViewById(R.id.ivBack);
+	}
+	private void initEvent(){
+		confirm_btn.setOnClickListener(this);
+		ivBack.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.confirm_btn:
+			
+			break;
+		case R.id.ivBack:
+			
+			break;
+		}
+	}
+	
 }
--- src/com/gizwits/ledgateway/activity/EditGroupActivity.java
+++ src/com/gizwits/ledgateway/activity/EditGroupActivity.java
@@ -1,7 +1,47 @@
 package com.gizwits.ledgateway.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import com.gizwits.framework.activity.BaseActivity;
+import com.gizwits.ledgateway.R;
 
-public class EditGroupActivity extends BaseActivity {
+public class EditGroupActivity extends BaseActivity implements OnClickListener {
 
+	/** The iv back. */
+	private ImageView ivBack;
+	/** The ll scroll layout. */
+	private LinearLayout ll_scroll;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_group);
+		initViews();
+		initEvent();
+		setItemTollScroll();
+	}
+	
+	private void initViews(){
+		ivBack = (ImageView) findViewById(R.id.ivBack);
+		ll_scroll = (LinearLayout) findViewById(R.id.ll_scroll);
+	}
+	
+	private void initEvent(){
+		ivBack.setOnClickListener(this);
+	}
+	
+	//设置滑动layout中子项
+	private void setItemTollScroll(){
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
 }
