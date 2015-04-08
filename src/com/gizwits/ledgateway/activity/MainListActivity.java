b073cd2f9d2fc216032e65a09941659deead8c0c
sunny <ytding@xtremeprog.com>
2015-04-08 12:05:56 +0800

    bug fix

--- src/com/gizwits/ledgateway/activity/MainListActivity.java
+++ src/com/gizwits/ledgateway/activity/MainListActivity.java
@@ -985,7 +985,7 @@ public class MainListActivity extends BaseActivity implements OnClickListener {
 		} else {
 			if (mXpgWifiDevice != null && mXpgWifiDevice.isConnected()) {
 				mCenter.cDisconnect(mXpgWifiDevice);
-				mXpgWifiDevice = null;
+				DisconnectOtherDevice();
 			}
 			finish();
 		}
